d17ceb535e52840cd170e042f77dd6b7f76de4e7
TomR809 <littlemanbigwalls@gmail.com>
2025-02-20 13:54:29 +0000

    chat enlarged formatting fix

--- obj/Debug/net48/RevitChatBotPrototype1.AssemblyInfo.cs
+++ obj/Debug/net48/RevitChatBotPrototype1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RevitChatBotPrototype1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc2d54cd7852c4b3341c9eec32e44121cfef22f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651bbc1feb81d0a6e73cb38e4b806f3baac14d8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RevitChatBotPrototype1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RevitChatBotPrototype1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
