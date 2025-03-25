19e1b988578eb1d4363e2c7446f7c187263e5d8a
Aaron <ewass04@sky.com>
2025-03-25 00:42:41 +0000

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
-import pkg from 'eslint';
-const { defineConfig } = pkg;
-
-export default defineConfig({
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  env: {
-    browser: true,
-    node: true,
-  },
-});
+module.exports = {
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    env: {
+      browser: true,
+      node: true,
+    },
+  };
+  
