1ec95b6981df507a84aaeb8024d5eefaf7b11c7a
olly <ollyjprince2005@gmail.com>
2025-03-27 13:50:43 +0000

    added first version of installer

---
+++ installer.ps1
@@ -0,0 +1,13 @@
+$policy = Get-ExecutionPolicy
+Set-ExecutionPolicy RemoteSigned
+fnm install 22
+node -v
+npm -v #installed npm/node      #ollama,axios,nodejs,express,electron,
+
+npm i electron
+npm i axios
+npm i express fs path
+npm i jest
+
+
+Set-ExecutionPolicy $policy
